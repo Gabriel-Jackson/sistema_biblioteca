3f18c44ab259798dfb1ea2bbdbf9dd024793ac85
Gabriel-Jackson <gabrielchrists@gmail.com>
2019-12-07 19:10:32 -0300

    Interface básica com tabela dos livro

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Sistema Bibliotecário</title>
+</head>
+<body>
+  <table>
+  <thead>
+  <th>Livro</th>
+  <th>Autor</th>
+  <th>Status</th>
+  <th>...</th>
+  </thead>
+  <tbody>
+  <td>Percy Jackson e o Ladrão de Raios</td>
+  <td>Rick Riordan</td>
+  <td>Disponível</td>
+  <td>...</td>
+  </tbody>
+  </table>
+</body>
+</html>
