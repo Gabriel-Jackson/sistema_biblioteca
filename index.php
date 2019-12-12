f11e42969ccbaa36d07a5e95e69c97911ce22ded
Gabriel-Jackson <gabrielchrists@gmail.com>
2019-12-12 17:55:19 -0300

    Recebendo livros do DB e jogando na tabela

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+  require_once ("config/connect.php");
+
+  $livros = $connect->query("SELECT * FROM livros");
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Sistema Bibliotecário</title>
+</head>
+<body>
+  <table>
+  <thead>
+  <th>Livro</th>
+  <th>Autor</th>
+  <th>Status</th>
+  <th>...</th>
+  </thead>
+  <tbody>
+  <?php
+  foreach ($livros->fetchAll() as $livro ) {
+    echo  "<tr>";
+    echo "<td>".$livro[titulo]."</td>";
+    echo "<td>".$livro[autor]."</td>";
+    echo "<td>Disponível</td>";
+    echo "<td>...</td>";
+    echo "</tr>";
+  }
+  /*<td>Percy Jackson e o Ladrão de Raios</td>
+  <td>Rick Riordan</td>
+  */
+  ?>
+  </tbody>
+  </table>
+</body>
+</html>
