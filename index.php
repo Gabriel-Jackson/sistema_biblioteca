22ef45424f3f7031d5276b1150e2bb78aea21a94
Gabriel-Jackson <gabrielchrists@gmail.com>
2019-12-14 04:50:53 -0300

    Paginaçao

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php 
-
+  
   require_once ("config/connect.php");
+  
+  
+  $max_entries = 3;
+  
+  
+  $todoslivros = $connect->query("SELECT * FROM livros ");
 
-  $livros = $connect->query("SELECT * FROM livros");
+  $num_pags = $todoslivros->rowCount()/$max_entries;
+
+  if(!isset($_GET['pagina']) || empty($_GET['pagina'])){
+    $pagina = 0;
+  }else{
+    $pagina =  intval($_GET['pagina']);
+  }
+
+  $livros = $connect->query("SELECT * FROM livros
+  LIMIT ".($pagina*$max_entries).",$max_entries");
 ?>
 
 
@@ -16,6 +31,8 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+  
   <table>
   <thead>
   <th>Livro</th>
@@ -25,24 +42,37 @@
   </thead>
   <tbody>
   <?php
-  foreach ($livros->fetchAll() as $livro ) {
-    echo  "<tr border='1px'>";
-    echo "<td>".$livro[titulo]."</td>";
-    echo "<td>".$livro[autor]."</td>";
-    echo "<td>Disponível</td>";
-    echo "<td>  ...  </td>";
-    echo "</tr>";
-  }
-  /*<td>Percy Jackson e o Ladrão de Raios</td>
-  <td>Rick Riordan</td>
-  */
+    foreach ($livros->fetchAll() as $livro ) {
+      echo  "<tr border='1px'>";
+      echo "<td>".$livro[titulo]."</td>";
+      echo "<td>".$livro[autor]."</td>";
+      echo "<td>Disponível</td>";
+      echo "<td>  ...  </td>";
+      echo "</tr>";
+    }
+    /*<td>Percy Jackson e o Ladrão de Raios</td>
+    <td>Rick Riordan</td>
+    */
   ?>
   </tbody>
   </table>
   <div>
-  <a href="#"><-</a>
-  <a href="#">...</a>
-  <a href="#">-></a>
+  <?php
+    if($pagina > 0 && $num_pags != 1){
+    echo "<a href='".$_SERVER['PHP_SELF'].
+        "?pagina=".($pagina-1)."'> <- </a>"; 
+    }
+    for ($i = 0; $i < $num_pags; $i ++) {
+      echo "<a href='".$_SERVER['PHP_SELF'].
+      "?pagina=$i'> ".($i+1) ."</a>";
+    }
+    if($pagina < $num_pags-1 && $num_pags != 1){
+      echo "<a href='".$_SERVER['PHP_SELF'].
+          "?pagina=".($pagina+1)."'> -> </a>"; 
+      }
+
+      
+  ?>
   </div>
 </body>
 </html>
