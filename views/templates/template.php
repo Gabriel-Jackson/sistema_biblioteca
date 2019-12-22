9b7b103f942e0d2866d999add5a3bc723f234f8d
Jack <gabrielchrists@gmail.com>
2019-12-22 20:09:19 -0300

    Refactoring para modelo MVC

--- views/templates/template.php
+++ views/templates/template.php
@@ -9,7 +9,30 @@
     <link rel="stylesheet" href="/public/css/style.css"> --->
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0" action="" method="GET">
+            <div class="input-group">
+            <input class="form-control bg-dark text-light" name="term" type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-dark border my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+            </div>
+            </div>
+        </form>
+    </div>
+    </nav>
     <?php $this->loadViewInTemplate($viewName, $viewData); ?>
     <!--- <script src="/public/js/jquery.js"></script>
     <script src="/public/js/bootstrap.bundle.js"></script> -->
