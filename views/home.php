2309a01cee4cbf49f2b3d2cad5c5fb88ba54fcec
Jack <gabrielchrists@gmail.com>
2019-12-23 13:10:01 -0300

    Refactoring para modelo MVC

--- views/home.php
+++ views/home.php
@@ -1,11 +1,20 @@
-<form class="form-inline">
-    <div class="form-group">
-    <label for="numMax">Livros por Página:</label>
-    
-    <input type="text" name="numMax" id="numMax" value="5">
+<form class="form-check-inline border rounded p-2" id="pageForm">
+    <span class="mr-2">Livros por Página: </span>
+    <div class="form-check form-check-inline ml-1 " name="firstRadio">
+        <input class="form-check-input" type="radio" name="limit" id="numMax5" value="5">
+        <label class="form-check-label" for="numMax5">5</label>
+    </div>
+    <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="limit" id="numMax10" value="10">
+            <label class="form-check-label" for="numMax10">10</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="limit" id="numMax20" value="20">
+        <label class="form-check-label" for="numMax20">20</label>
     </div>
 </form>
-<table class="table table-stripped mt-3 mb-3">
+<input type="hidden" name="pag" id="pag" value="0">
+<table class="table table-stripped mt-3 mb-3 border">
     <thead class="thead">
         <tr>
             <th scope="col">Titulo</th>
@@ -14,16 +23,20 @@
             <th scope="col"><i class="fas fa-ellipsis-h"></i></th>
         </tr>
     </thead>
-    <tbody>
+    <tbody >
     <?php foreach ($livros as $livro):?>
         <tr>
-            <td scope="row"><?=$livro['titulo']?></td>
-            <td><?=$livro['autor']?></td>
-            <td><?= "Disponível" ?></td>
+            <td scope="row" class="titulo"><?=$livro['titulo']?></td>
+            <td class="autor"><?=$livro['autor']?></td>
+            <td class="status"><?= "Disponível" ?></td>
             <td><button class="btn"><i class="fas fa-ellipsis-h"></button></i></td>
         </tr>
     <?php endforeach; ?> 
     </tbody>
 </table>
 
+<nav aria-label="Page navigation">
+    <ul class="pagination" id="ulPagination">
+    </ul>
+</nav>
 <script src="public/js/pagination.js"></script>
--- views/templates/template.php
+++ views/templates/template.php
@@ -12,9 +12,15 @@
     <script src="public/js/bootstrap.bundle.js"></script>
 </head>
 <body>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        
+        <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark mb-3">
+            
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse aling-self-start" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -26,16 +32,37 @@
                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0" action="" method="GET">
-                    <div class="input-group">
-                    <input class="form-control bg-dark text-light" name="term" type="search" placeholder="Search" aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn btn-dark border my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                <form class="d-flex flex-column justify-content-center form-inline my-2 my-lg-0" action="" method="GET" name="filtro">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input class="form-control bg-dark text-light" name="term" type="search" placeholder="Procurar livros..." aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-dark border my-2 my-sm-0 mr-2 rounded-right" type="submit"><i class="fas fa-search"></i></button>
+                        </div>
+                        </div>
+                        <button class="btn btn-dark border my-2 my-sm-0" type="button" data-toggle="collapse" data-target="#opcoesFiltro" aria-expanded="true" aria-controls="opcoesFiltro"><i class="fas fa-filter"></i></button>
                     </div>
+                    
+                        <div class="aling-self-end form-group border p-3 my-2 collapse" id="opcoesFiltro">
+                            <div class="form-check form-check-inline text-light">
+                                <input id="opcaoAutor" class="form-check-input " type="radio" name="opcao" value="autor" >
+                                <label for="opcaoAutor" class="form-check-label">Autor</label>
+                            </div>
+                            <div class="form-check form-check-inline text-light">
+                                <input id="opcaoTitulo" class="form-check-input " type="radio" name="opcao" value="titulo" >
+                                <label for="opcaoTitulo" class="form-check-label">Titulo</label>
+                            </div>
+                            <div class="form-check form-check-inline text-light">
+                                <input id="opcaoStatus" class="form-check-input " type="radio" name="opcao" value="status" >
+                                <label for="opcaoStatus" class="form-check-label">Status</label>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
         </nav>
+        
+    <div class="container">
         <?php $this->loadViewInTemplate($viewName, $viewData); ?>
     </div>
 
