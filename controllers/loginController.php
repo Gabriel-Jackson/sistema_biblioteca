1a107cd4ebdabaa5617e37e4a0d1a831269ea0d6
Jack <gabrielchrists@gmail.com>
2020-01-04 20:16:46 -0300

    remover comentario desnecessario

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -10,19 +10,5 @@ class loginController extends controller
             header('Location: http://localhost:8090/');
         }
     }
-    /*public function validate (){
-        $user = new user();
-        $user->setUserName($_POST['user']);
-        $user->setUserPass($_POST['password']);
-
-        $userLogged = $user->validateUser();
-        if($userLogged != null){
-            $_SESSION['userName'] = $userLogged['name'];
-            $_SESSION['userId'] = $userLogged['id'];
-            $_SESSION['userPrivilege'] = $userLogged['privilege'];
-            header("Location: http://localhost:8090/");
-        }
-        
-    }*/
 }
 ?>
